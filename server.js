63b12918f887867035c8d8fefc3a4e104a86884c
khalidhmd <khalidhmd@outlook.com>
2018-07-31 20:57:26 +0200

    done

--- server.js
+++ server.js
@@ -24,9 +24,14 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/timestamp/:date_string?", function (req, res) {
+  let date = new Date(req.params.date_string);
+  if(date === NaN) res.json({UNIX: null, utc: 'Invalid Date'})
+  res.json({UNIX: date.getTime(), utc: date.toUTCString()});
+});
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT , function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
